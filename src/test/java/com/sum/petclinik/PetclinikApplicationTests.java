fbab3e64a3f4158646d7443d257854ba538df94c
Sumit Maji <skmaji1@outlook.com>
2020-09-06 12:25:35 +0530

    Initial Commit

---
+++ src/test/java/com/sum/petclinik/PetclinikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sum.petclinik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
