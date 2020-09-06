4b94ff9f8b3a42be115d7cc1b162bdd30c6cda6f
Sumit Maji <skmaji1@outlook.com>
2020-09-06 18:03:34 +0530

    Index Page for Owner. closes #14.

---
+++ petclinik-web/src/main/java/com/sum/petclinik/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package com.sum.petclinik.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners", "owners/index", "/owners/index.html"})
+    public String listVets(){
+        return "owners/index";
+    }
+}
