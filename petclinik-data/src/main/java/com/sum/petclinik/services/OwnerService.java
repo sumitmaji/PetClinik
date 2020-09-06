141a4a3fb02d2bb72a476296a5b168f9d5aec838
Sumit Maji <skmaji1@outlook.com>
2020-09-06 17:24:27 +0530

    Common Base Service. closes #11.

--- petclinik-data/src/main/java/com/sum/petclinik/services/OwnerService.java
+++ petclinik-data/src/main/java/com/sum/petclinik/services/OwnerService.java
@@ -4,9 +4,6 @@ import com.sum.petclinik.model.Owner;
 
 import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- petclinik-data/src/main/java/com/sum/petclinik/services/VetService.java
+++ petclinik-data/src/main/java/com/sum/petclinik/services/VetService.java
@@ -4,9 +4,5 @@ import com.sum.petclinik.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
